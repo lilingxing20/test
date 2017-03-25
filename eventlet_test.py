8f146a073bb24ecaa74558d9812bfa9bfa403a11
lixingxing <lixx@vsettan.com.cn>
2017-03-25 10:20:38 +0800

    eventlet test

---
+++ eventlet_test.py
@@ -0,0 +1,20 @@
+# --*-- coding: utf-8 --*--
+# author: zwei
+# email: lilingxing20@163.com
+import eventlet
+from eventlet.green import urllib2
+
+urls = [
+    "https://res3.lekan.com/video/13/47/57/E1/shot/5_314x224.jpg",
+    "https://res3.lekan.com/video/13/47/57/E2/shot/5_314x224.jpg",
+    "https://res3.lekan.com/video/13/47/57/E3/shot/5_314x224.jpg",
+]
+
+def fetch(url):
+    return urllib2.urlopen(url).read()
+
+pool = eventlet.GreenPool()
+
+for body in pool.imap(fetch, urls):
+    print("got body", len(body))
+
