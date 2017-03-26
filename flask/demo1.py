0284ebbf6a9ae869fb7ef59685d39fb14e64dbd3
lixingxing <lixx@vsettan.com.cn>
2017-03-27 00:49:07 +0800

    flask demo

---
+++ flask/demo1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding=utf-8
+# author: lixx
+# email: lilingxing20@163.com
+
+from flask import Flask
+from flask import abort
+from flask import redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Hello World!</h1>'
+
+@app.route('/user/<name>')
+def sayHello(name):
+    if name == 'baidu':
+        return redirect('http://www.baidu.com')
+    elif name == 'NO':
+        return abort(404)
+
+    return '<h1> Hello,%s </h1>' % name
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
